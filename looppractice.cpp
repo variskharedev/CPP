1062d89f6154a22df7e58435dbefea271f87dceb
variskhare <variskhare@gmail.com>
2026-05-11 23:35:37 +0530

    basics of looping

---
+++ looppractice.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+
+using namespace std;
+#include<string>
+
+// int main(){
+    
+//      cout<<"Here is count\n";
+
+//     for(int i=0; i<=100; i++){
+        
+       
+//         cout<<i<<endl;
+
+
+//     }
+// }
+
+// printing 0 to 100 with while loop
+// int main(){
+
+//     cout<<"Here is count with while loop";
+
+//     int i=0;
+
+//     while (i<=100)
+//     {
+//         cout<<i<<endl;
+//         i++;
+//     }
+    
+//     return 0;
+
+// }
+
+
+// printing even numbers from 0 to 50
+
+// int main(){
+
+//     int i = 0;
+
+//     while (i<=50)
+//     {
+//         if (i%2==0)
+//         {
+//             cout<<i<<endl;
+//         }
+        
+//         i++;
+//     }
+    
+//     return 0;
+// }
+
+
+// Creating multiplication table 
+
+// int main(){
+
+//     int number;
+
+//     cout<<"Enter a number\n";
+
+//     cin>>number;
+
+//     int i = 1;
+
+//     while(i<=10){
+
+//         cout<<number*i<<endl;
+//         i++;
+
+//     }
+//     return 0;
+// }
+
+// Sum of first n natural number
+
+
+// int main(){
+
+//     int n;
+
+//     cout<<"Enter number\n";
+
+//     cin>>n;
+
+//     int sum=0;
+
+//     for(int i = 1; i<=n; i++){
+
+//         sum = sum+i;
+
+//         //cout<<sum<<endl;
+
+//     }
+
+//     cout<<sum;
+
+//     return 0;
+// }
+
+
+// Check number of digits
+
+// int  main(){
+
+//     long long num;
+
+//     int count = 0;
+    
+//     cout<<"Enter a number\n";
+
+//     cin>>num;
+//     while (num!=0){
+
+//         num = num/10;
+//         count++;
+
+//     }
+  
+//     cout<<count;
+
+//     return 0;
+// }
+
+
+// Get the factorial of a number
+
+
+// int main(){
+
+//     int fact = 1;
+
+//     int n;
+
+//    cout<<"Enter the number\n";
+
+//    cin>>n;
+
+//     for(int i=1; i<=n;i++){
+     
+//         fact = fact*i;
+        
+//     }
+
+//     cout<<fact;
+    
+//     return 0;
+// }
+
+
+// Reverse the given number
+
+// int main(){
+   
+//     int temp;
+//     int hold;
+//     int num;
+
+//     cout<<"Enter a number\n";
+
+//     cin>>num;
+
+//     while(num!=0){
+     
+//        hold = num % 10;
+
+//        num = num/10;
+       
+//        cout<<hold;
+
+//     }
+
+//     return 0;
+// }
+
+
+
+//
+
+
+// Fabonaci series using do while
+
+int main(){
+  
+    int fab = 1;
+    int i = 0;
+
+    do
+    {
+        
+    }while();
+    
+    
+    return 0;
+}
